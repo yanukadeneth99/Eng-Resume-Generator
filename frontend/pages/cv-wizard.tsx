ba2352f08cdc5e73691effcd6088dd68dd61705c
Malaka Silva <malakasilva43@gmail.com>
2023-05-21 13:28:54 +0530

    permission error page added

--- frontend/pages/cv-wizard.tsx
+++ frontend/pages/cv-wizard.tsx
@@ -13,19 +13,28 @@ import { Viewer, Worker } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 import { useAuth } from "@/context/AuthContext";
+import { RxReload } from "react-icons/rx";
+import { FaDownload } from "react-icons/fa";
+import PermissionError from "@/components/PermissionError";
+
+const getFormattedDate = () => {
+  let date = new Date();
+
+  return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+};
 
 const CvWizard: NextPage = () => {
   const router = useRouter();
   const [currentStep, setCurrentStep] = useState(1);
   const [data, setData] = useState({});
   const { isAuthenticated, checkAuthenticated } = useAuth();
+  const [user, setUser] = useState("");
+  const [_time, _setTime] = useState(getFormattedDate());
 
   useEffect(() => {
     checkAuthenticated();
 
-    if (!isAuthenticated) {
-      router.push("/");
-    }
+    setUser(localStorage.getItem("user") || "");
   }, [checkAuthenticated]);
 
   let _next = (fData: any) => {
@@ -46,65 +55,87 @@ const CvWizard: NextPage = () => {
     }
   };
 
-  return (
-    <>
-      <Head>
-        <title>CV Wizard | Resume Generator</title>
-        <meta name="description" content="Login to Create your own Resume" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header isHome={false} />
+  if (isAuthenticated) {
+    return (
+      <>
+        <Head>
+          <title>CV Wizard | Resume Generator</title>
+          <meta name="description" content="Login to Create your own Resume" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header isHome={false} />
 
-      <div className="h-[calc(100vh-120px)] mt-[120px]">
-        <div
-          className="w-[80%] h-[90%] bg-white  relative top-[50%] left-[50%] px-8 theme-blue theme font-sans"
-          style={{ transform: "translate(-50%, -50%)" }}
-        >
-          <div className="h-full grid grid-cols-2 gap-16">
-            <div className="overflow-auto h-full shadow-2xl rounded-xl">
-              <div className="pb-16">
-                <div>
-                  <PersonalDetails
-                    currentStep={currentStep}
-                    _prev={_prev}
-                    _afterValid={_next}
-                  />
-                  <WorkDetails
-                    currentStep={currentStep}
-                    _prev={_prev}
-                    _afterValid={_next}
-                  />
-                  <EducationalDetails
-                    currentStep={currentStep}
-                    _prev={_prev}
-                    _afterValid={_next}
-                  />
-                  <Skills
-                    currentStep={currentStep}
-                    _prev={_prev}
-                    _afterValid={_next}
-                  />
+        <div className="h-[calc(100vh-120px)] mt-[120px]">
+          <div
+            className="w-[80%] h-[90%] bg-white  relative top-[50%] left-[50%] px-8 theme-blue theme font-sans"
+            style={{ transform: "translate(-50%, -50%)" }}
+          >
+            <div className="h-full grid grid-cols-2 gap-16">
+              <div className="overflow-auto h-full shadow-2xl rounded-xl">
+                <div className="pb-16">
+                  <div>
+                    <PersonalDetails
+                      currentStep={currentStep}
+                      _prev={_prev}
+                      _afterValid={_next}
+                    />
+                    <WorkDetails
+                      currentStep={currentStep}
+                      _prev={_prev}
+                      _afterValid={_next}
+                    />
+                    <EducationalDetails
+                      currentStep={currentStep}
+                      _prev={_prev}
+                      _afterValid={_next}
+                    />
+                    <Skills
+                      currentStep={currentStep}
+                      _prev={_prev}
+                      _afterValid={_next}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="overflow-auto h-full">
-              <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.js">
-                <div
-                  style={{
-                    marginLeft: "auto",
-                    marginRight: "auto",
-                  }}
+              <div className="overflow-auto h-full">
+                <a
+                  href={`${process.env.NEXT_PUBLIC_SERVER_APP}/pdf/generate/${user}/${_time}`}
                 >
-                  <Viewer fileUrl="/cv-sample.pdf" />
-                </div>
-              </Worker>
+                  <FaDownload className="bg-primary border-2 z-50 absolute right-[-50px] top-0 cursor-pointer border-primary text-[45px] text-[white] rounded-full w-fit py-2" />
+                </a>
+
+                <RxReload
+                  className="bg-primary border-2 z-50 absolute right-[-50px] top-[60px] cursor-pointer border-primary text-[45px] text-[white] rounded-full w-fit py-2"
+                  onClick={() => _setTime(getFormattedDate())}
+                />
+
+                {_time ? (
+                  <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.js">
+                    <div
+                      style={{
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                      }}
+                    >
+                      <Viewer
+                        fileUrl={`${process.env.NEXT_PUBLIC_SERVER_APP}/pdf/generate/${user}/${_time}`}
+                      />
+                    </div>
+                  </Worker>
+                ) : (
+                  ""
+                )}
+                <p className="text-center">Last updated: {_time}</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    return <PermissionError />;
+  }
 };
 
 export default CvWizard;
